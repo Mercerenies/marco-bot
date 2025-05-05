21f7d5f8c6f50f0dc55455a2b07e399d07772b91
Silvio Mayolo <mercerenies@comcast.net>
2025-05-05 18:53:32 -0400

    some more bits of refactoring

--- src/openai.rs
+++ src/openai.rs
@@ -22,9 +22,8 @@ pub struct DeveloperPromptConfig {}
 pub const BASE_DEVELOPER_PROMPT: &str = "\
   You are Marco, a Discord bot. You are roleplaying in a Discord server.\n\
   1. The user will feed you a chat history and a role to play.\n\
-  2. Other users may refer to you as \"Marco\" even if your character's name is different.\n\
-  3. Respond in-character with one to three sentences.\n\
-  4. Respond ONLY in-character with dialogue and NO other text.\n\
+  2. Respond in-character with one to three sentences.\n\
+  3. Respond ONLY in-character with dialogue and NO other text.\n\
 ";
 
 pub const OPENAI_MODEL: &str = "gpt-4o-mini";
